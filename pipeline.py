be1e21cfc4251a7fb5f953d06b18ac40d351ffcb
Laura <laura.ventrice@edu.unito.it>
2023-04-01 20:04:09 +0200

    added eval for no trf 1

--- pipeline.py
+++ pipeline.py
@@ -21,7 +21,7 @@ path_articles2_titles = f'input/articles2/articles2_title.txt'
 
 path_results = f'results/extraction_entities_snippet/'
 
-titles_articles = read_titles(path_articles1_titles, lang) # TODO: ARRIVATI A TORONTO ESCLUSO!!
+titles_articles = read_titles(path_articles1_titles, lang)
 
 model = create_model(lang, trf)
 
